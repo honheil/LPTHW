b060fa86fcabee4b8e9e4df9ee5a14f001a21e32
Hon Hei Lam <honheil@Terrences-MacBook-Pro.local>
2016-10-25 15:01:40 +0200

    added week 1 exercises

---
+++ ex10.py
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+# \t -> horizontal tab (開新行但係唔置左)
+# \n -> 開新行
